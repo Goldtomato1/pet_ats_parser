269da823c29b2c5dfebb3d5d13c0ccdb7ca4f5a8
Goldtomato1 <goldtomato1@gmail.com>
2022-11-29 23:16:40 +0300

    added function min_values and plot

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 from ats_parser import df_from_ats
 from df_db import df_to_pg, pg_to_df
-import pandas as pd
+from matplotlib import pyplot as plt
 
-pd.options.plotting.backend = "plotly"
 
 
 def plotly_creator(func):
-    def show(*args, **kwargs):
-        df = func(*args, **kwargs)
-        fig = df.plot()
-        return fig.show()
-    return show
+    def wrapper(*args, **kwargs):
+        fig = func(*args, **kwargs).plot()
+        return plt.show()
+    return wrapper
 
 @plotly_creator
 def max_values(df):
     return df.resample('D').max().dropna().drop('hour', axis=1)
- #   return df[['date', 'val']].groupby('date').max('val')
+
+
+
 @plotly_creator
-def mean_values(df):
-    return df.resample('D').mean().dropna().drop('hour', axis=1)
+def min_values(df):
+    return df.resample('D').min().dropna().drop('hour', axis=1)
 
 def main():
-    #df = df_from_ats()
-    #df_to_pg(df)
+    df = df_from_ats()
+    df_to_pg(df)
     df_from_db = pg_to_df()
     max_day = max_values(df_from_db)
+    min_day = min_values(df_from_db)
 
 
 if __name__ == '__main__':
