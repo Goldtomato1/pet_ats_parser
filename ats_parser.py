28fa34f376516aacb151af7e41ff2f33d4ec2308
Goldtomato1 <goldtomato1@gmail.com>
2022-12-14 13:53:40 +0300

    added function min_values and plot

--- ats_parser.py
+++ ats_parser.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import pandas as pd
 import requests
+from datetime import date, datetime
 
 
 def df_from_ats():
@@ -14,13 +15,86 @@ def df_from_ats():
     options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
     driver = webdriver.Firefox(executable_path='geckodriver.exe', options=options)
     driver.get(url)
-    delay = 10 # seconds
+    delay = 10  # seconds
     try:
         WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'xml-data-row')))
-        print ("Page is ready!")
+        print("Page is ready!")
     except TimeoutException:
-        print ("Loading took too much time!")
+        print("Loading took too much time!")
 
     dwnld_link = driver.find_element_by_xpath("//a[contains(text(),'Московская область')]").get_attribute('href')
     req = requests.get(dwnld_link, verify=False)
     return pd.read_excel(req.content, names=['date', 'hour', 'val'], skiprows=6)
+
+
+def big_nodes_prices():
+    options = Options()
+    options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+    driver = webdriver.Firefox(executable_path='geckodriver.exe', options=options)
+    start_date = date(2022, 9, 1)
+    end_date = date(2022, 10, 31)
+    dates = pd.date_range(start_date, end_date, freq='d').strftime('%Y%m%d').tolist()
+    columns_name = ['date', 'hour', 'id_node', 'name_node', 'u_nom', 'u_fact', 'name_subj', 'price']
+    df = pd.DataFrame(columns=columns_name)
+
+    for day in dates:
+        url = f'https://www.atsenergo.ru/nreport?rname=big_nodes_prices_pub&region=eur&rdate={day}'
+        driver.get(url)
+        delay = 10  # seconds
+        try:
+            WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'reports_breadcrumb')))
+            print("Page is ready!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        dwnld_link = driver.find_element_by_xpath(
+            f"//a[contains(text(),'{day}_eur_big_nodes_prices_pub.xls')]").get_attribute('href')
+        req = requests.get(dwnld_link, verify=False)
+
+        dict_df = pd.read_excel(req.content, sheet_name=None, names=columns_name[2:], usecols='A:F', skiprows=2)
+
+        for k, v in dict_df.items():
+            v['hour'] = k
+            v['date'] = datetime.strptime(day, '%Y%m%d').date()
+            df = pd.concat([df, v], ignore_index=True)
+    return df
+
+
+def sell_units():
+    options = Options()
+    options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+    driver = webdriver.Firefox(executable_path='geckodriver.exe', options=options)
+    start_date = date(2022, 9, 1)
+    end_date = date(2022, 10, 31)
+    dates = pd.date_range(start_date, end_date, freq='d').strftime('%Y%m%d').tolist()
+    columns_name = ['date', 'hour', 'id_gen', 'name_gen', 'id_node', 'name_node', 'tech_min', 'technol_min',
+                    'down_limit', 'plan_vol', 'up_limit', 'tech_max', 'technol_max']
+    df = pd.DataFrame(columns=columns_name)
+
+    for day in dates:
+        url = f'https://www.atsenergo.ru/nreport?rname=carana_sell_units&region=eur&rdate={day}'
+        driver.get(url)
+        delay = 10  # seconds
+        try:
+            WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'reports_files')))
+            print("Page is ready!")
+        except TimeoutException:
+            print("Loading took too much time!")
+
+        dwnld_link = driver.find_element_by_xpath(
+            f"//a[contains(text(),'{day}_MOSENERG_eur_sell_units.xls')]").get_attribute('href')
+        req = requests.get(dwnld_link, verify=False)
+
+        df_excel = pd.read_excel(req.content, header=None, skiprows=7, skipfooter=1)
+
+        for h, i in zip(range(0, 24), range(4, 178, 7)):
+            v = pd.concat([df_excel.iloc[:, 0:4], df_excel.iloc[:, i:i + 7]], axis=1)
+            v.columns = columns_name[2:]
+            v['hour'] = h
+            v['date'] = datetime.strptime(day, '%Y%m%d').date()
+
+            df = pd.concat([df, v], ignore_index=True)
+    return df
+
+
+
--- df_db.py
+++ df_db.py
@@ -32,3 +32,70 @@ def pg_to_df():
     db = create_engine(conn_string)
     conn = db.connect()
     return pd.read_sql_table('fact_region', conn, index_col='date')
+
+
+def big_nodes_prices_to_pg(df):
+    conn_string = 'postgresql://postgres:qawsed@127.0.0.1:5434/ats_parse'
+    db = create_engine(conn_string)
+    conn = db.connect()
+    conn1 = psycopg2.connect(
+        database="ats_parse",
+        user='postgres',
+        password='qawsed',
+        host='127.0.0.1',
+        port='5434'
+    )
+    conn1.autocommit = True
+    cursor = conn1.cursor()
+    cursor.execute('drop table if exists big_nodes_prices_pub')
+    sql = '''
+        CREATE TABLE big_nodes_prices_pub(
+            date date,
+            hour INT,
+            id_node INT,
+            name_node VARCHAR(120),
+            u_nom INT,
+            u_fact numeric(10,3),
+            name_subj VARCHAR(120),
+            price numeric(10,3)
+            );
+    '''
+    cursor.execute(sql)
+    df.to_sql('big_nodes_prices_pub', conn, index=False, if_exists='append')
+
+
+
+def sell_units_to_pg(df):
+    conn_string = 'postgresql://postgres:qawsed@127.0.0.1:5434/ats_parse'
+    db = create_engine(conn_string)
+    conn = db.connect()
+    conn1 = psycopg2.connect(
+        database="ats_parse",
+        user='postgres',
+        password='qawsed',
+        host='127.0.0.1',
+        port='5434'
+    )
+    conn1.autocommit = True
+    cursor = conn1.cursor()
+    cursor.execute('drop table if exists sell_units')
+    sql = '''
+        CREATE TABLE sell_units(
+            date date,
+            hour INT,
+            id_gen INT,
+            name_gen VARCHAR(120),
+            id_node INT,
+            name_node VARCHAR(120),
+            tech_min numeric(10,3),
+            technol_min numeric(10,3),
+            down_limit numeric(10,3),
+            plan_vol numeric(10,3),
+            up_limit numeric(10,3),
+            tech_max numeric(10,3),
+            technol_max numeric(10,3)
+            );
+    '''
+    cursor.execute(sql)
+    df.to_sql('sell_units', conn, index=False, if_exists='append')
+
